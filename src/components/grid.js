cb6881438bab1145267d65053b8d972383094c44
Andrew Jose <ajosedev@gmail.com>
2018-07-14 15:11:45 +1000

    Grid component now renders 9 GridItems

--- src/components/grid.js
+++ src/components/grid.js
@@ -1,17 +1,29 @@
 import React from "react";
 import styled from "styled-components";
+import GridItem from './gridItem';
 
 const StyledGrid = styled.div`
   display: grid;
+  grid-template-columns: repeat(3, auto);
+  grid-gap: .2rem;
   border: 1px solid black;
   border-radius: 2px;
   width: 3rem;
   height: 3rem;
 `;
 
+const renderGridItems = () => {
+  const gridItems = [];
+  for (let i = 0; i < 9; i++) {
+    gridItems.push(<GridItem key={i} />);
+  }
+
+  return gridItems;
+}
+
 const Grid = () => (
   <StyledGrid>
-    Grid
+    {renderGridItems()}
   </StyledGrid>
 )
 
---
+++ src/components/gridItem.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const Item = styled.div`
+  background-color: red;
+  border-radius: 2px;
+`;
+
+const GridItem = () => (
+  <Item />
+)
+
+export default GridItem;
