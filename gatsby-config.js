67c4552059e47cd0cf355a2c2f4a1a76c1c30555
Andrew Jose <ajosedev@gmail.com>
2018-07-14 21:20:12 +1000

    Added site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,8 @@
 module.exports = {
+  siteMetadata: {
+    title: 'CSS Grid Cheatsheet',
+    siteUrl: 'https://grid.ajosedev.com',
+    description: 'A cheat sheet for all the common CSS Grid properties',
+  },
   plugins: [`gatsby-plugin-styled-components`],
 };
